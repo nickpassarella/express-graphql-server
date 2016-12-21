87524efdcb2e6a3ce72ca68216380f1a9128758d
Nick <nickpassarella@gmail.com>
2016-12-21 18:03:31 -0500

    Sets up a basic graphql schema and express server.

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { graphql, buildSchema } = require('graphql');
+
+const PORT = process.env.port || 3000;
+const server = express();
+
+const schema = buildSchema(`
+type Video {
+  id: ID,
+  title: String,
+  duration: Int,
+  watched: Boolean,
+}
+
+type Query {
+  video: Video
+  videos: [Video]
+}
+
+type Schema {
+  query: Query
+}
+`);
+
+const videoA = {
+  id: 'a',
+  title: 'video one',
+  duration: 120,
+  watched: true
+};
+
+const videoB = {
+  id: 'b',
+  title: 'video two',
+  duration: 240,
+  watched: false
+};
+
+const videos = [videoA, videoB];
+
+const resolvers = {
+  video: () => ({
+    id: '1',
+    title: 'bar',
+    duration: 180,
+    watched: true,
+  }),
+  videos: () => videos,
+};
+
+server.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+  rootValue: resolvers,
+}));
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+})
